67d557ba97725d047a88eeba07d239d52b92bbd2
Buwaneka De Silva <buwanekapasan@gmail.com>
2018-10-29 00:36:23 +0530

    Update OFFFastStartup.bat

--- OFFFastStartup.bat
+++ OFFFastStartup.bat
@@ -5,9 +5,8 @@
 :: Tutorial: http://www.tenforums.com/tutorials/4189-fast-startup-turn-off-windows-10-a.html 
 
 
-:: To enable hibernate
-powercfg -h on
+:: To turn off Fast Startup
+REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /V HiberbootEnabled /T REG_dWORD /D 0 /F
 
 
-:: To turn on Fast Startup
-REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /V HiberbootEnabled /T REG_dWORD /D 1 /F
+
