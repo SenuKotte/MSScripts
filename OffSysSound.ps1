098755db7774b2a3d38375e94a23b8f177065a36
Buwaneka De Silva <19805825+bpdesilva@users.noreply.github.com>
2019-03-09 02:05:54 +0530

    Update OffSysSound.ps1

--- OffSysSound.ps1
+++ OffSysSound.ps1
@@ -1,5 +1,5 @@
-$ThemeSounds = Get-ChildItem hkcu:\AppEvents\Schemes\Apps -Recurse | Get-ItemProperty
-foreach ($regkey in $ThemeSounds){
+$someSounds = Get-ChildItem hkcu:\AppEvents\Schemes\Apps -Recurse | Get-ItemProperty
+foreach ($regkey in $someSounds){
     $strVal = [string]$regkey.'(default)'
     if($strVal.EndsWith(".wav")){
         Set-ItemProperty -Path $regkey.PSPath -name "(default)" -Value ""
