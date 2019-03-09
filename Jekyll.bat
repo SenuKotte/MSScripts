9d36faf6080152c739e75d0d48a084e95863c5f0
Buwaneka De Silva <19805825+bpdesilva@users.noreply.github.com>
2019-03-10 01:53:35 +0530

    Create Jekyll.bat

---
+++ Jekyll.bat
@@ -0,0 +1,22 @@
+@echo off
+
+whoami /groups | find "S-1-16-12288" > nul
+
+if %errorlevel% == 0 (
+ echo Welcome, Admin
+  if(Test-Path -Path "$env:ProgramData\Chocolatey") {
+    echo Installing ruby
+    choco install ruby -y
+    gem install bundler
+    gem install jekyll
+    }
+   else {
+    echo Installing Chocolatey
+    @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+    choco install ruby -y
+    gem install bundler
+    gem install jekyll
+   }
+) else (
+ echo Get lost, User
+)
